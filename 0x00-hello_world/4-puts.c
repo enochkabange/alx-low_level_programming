5d78196ee16d0f328c5e324acfe4fb4db39f8fee
enigma <enochkabange@gmail.com>
2023-10-05 13:45:33 -0700

    put message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *main -this represents the main function
- *To Return: 0
-*/
-int main (void)
+ *  main - prints a given phrase
+ *  Return: 0 if exited properly, non-zero otherwise
+ */
+
+int  main(void)
 {
-  puts("/"Programming is like building a multilingual puzzle");
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
