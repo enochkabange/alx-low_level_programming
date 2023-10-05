5cda1fabaaff326715ba3b462f368962afd60358
enigma <enochkabange@gmail.com>
2023-10-05 13:48:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Descriptio of code purpose:to  print "with proper grammar, but the outcome is a piece
+ * of art,"
+ * Return: Always 0 (When Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
