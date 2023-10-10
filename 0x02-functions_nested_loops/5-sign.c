f39d4b835a7bae103616d901a2ef2e04c8909fe3
enigma <enochkabange@gmail.com>
2023-10-10 15:33:28 -0700

    prints signof a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - function prints the sign of the number
+ * @n: the int to check
+ * Return: 1 and prints + if n > zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
