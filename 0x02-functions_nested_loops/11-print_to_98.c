bd1927c767569f4356e3a661a416909271c7edfd
enigma <enochkabange@gmail.com>
2023-10-10 17:44:08 -0700

    multiples

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,29 @@
+#include "main.h"
 #include <stdio.h>
 
-/**                                                                 
- * main - compute and print sum of all the multiples                
- * of 3 | 5 below 1024                                              
- * Return: Always 0                                                 
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-    unsigned long int sum3, sum5, sum;
-    int i;
 
-    sum3 = 0;
-    sum5 = 0;
-    sum = 0;
-
-    for (i = 0; i < 1024; ++i)
-    {
-        if ((i % 3) == 0)
-        {
-            sum3 = sum3 + i;
-        } else if ((i % 5) == 0)
-        {
-            sum5 = sum5 + i;
+void print_to_98(int n) {
+    if (n <= 98) {
+        for (; n < 98; n++) {
+            printf("%d, ", n);
+        }
+    } else {
+        for (; n > 98; n--) {
+            printf("%d, ", n);
         }
     }
-    sum = sum3 + sum5;
-    printf("%lu\n", sum);
+    printf("98\n");
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    print_to_98(n);
     return (0);
 }
