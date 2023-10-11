a0a6758781a8a24f2bd92d7824cc063806c74b87
enigma <enochkabange@gmail.com>
2023-10-10 18:14:52 -0700

    added comment for function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet(void) /* print_alphabet - Prints the lowercase alphabet followed by a newline.*/
 {
 	char c = 'a';
 
