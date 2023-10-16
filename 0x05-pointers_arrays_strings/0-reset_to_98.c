62fb2ed811aa6887d7d949770fc9cfbf123a58f2
enigma <enochkabange@gmail.com>
2023-10-16 12:18:03 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of n to 98
+ * @n: integer pointer to variable to be updated
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my alx directory where all my pointers arrays and strings tasks for c language are stored
