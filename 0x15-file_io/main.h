89462c396ea57d7fa96fce2621e1a8b42165af49
Enoch W. Kabange <enochkabange@gmail.com>
2024-08-28 08:15:52 -0700

    changes made to main.h and 0-file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,18 @@
-#define __MAIN_H__
-#ifndef __MAIN_H__
-
-#include <unistd.h>
+File Edit Options Buffers Tools C Help                                                                                                                                                                             
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
+#ifndef MAIN_H
+#define MAIN_H
+
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
