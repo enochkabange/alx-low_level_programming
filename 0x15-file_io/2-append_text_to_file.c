c90a2c1e57f046a92520282a11c94858fcbd3708
Enoch W. Kabange <enochkabange@gmail.com>
2024-08-28 09:36:06 -0700

    2 append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,13 +2,12 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: filename
+ * @filename: filename.
  * @text_content: added content.
  *
  * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails
+ * or if it fails.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
